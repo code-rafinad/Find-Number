01a3881518a0cfc65db1a9d496caf1069e7f4f74
Rafinad <rafinad@outlook.com>
2021-04-30 00:12:41 +0300

    change hide buttons to change alpha

--- Find Number/Find Number/GameViewController.swift
+++ Find Number/Find Number/GameViewController.swift
@@ -43,7 +43,9 @@ setupScreen()
         
         for index in game.items.indices{
             buttons[index].setTitle(game.items[index].title, for: .normal)
-            buttons[index].isHidden = false
+            //buttons[index].isHidden = false
+            buttons[index].alpha = 1
+            buttons[index].isEnabled = true
         }
         
         nextDigit.text = game.nextItem?.title
@@ -51,7 +53,10 @@ setupScreen()
     
     private func updateUI(){
         for index in game.items.indices{
-            buttons[index].isHidden = game.items[index].isFound
+            //buttons[index].isHidden = game.items[index].isFound
+            buttons[index].alpha = game.items[index].isFound ? 0 : 1
+            buttons[index].isEnabled = !game.items[index].isFound
+            
             if game.items[index].isError{
                 UIView.animate(withDuration: 0.3) { [weak self] in
                     self?.buttons[index].backgroundColor = .red
